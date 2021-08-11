e81297e8bf9c592437919702c02cd7d725379270
Vilius <polerisviliux@gmail.com>
2021-08-11 16:51:06 +0300

    Cleaning project directory

---
+++ src/test/java/com/barcodegenerator/barcodegenerator/controller/HomeControllerIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.barcodegenerator.barcodegenerator.controller;
+
+import com.barcodegenerator.barcodegenerator.repository.QrBarcodeRepository;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HomeControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private QrBarcodeRepository qrBarcodeRepository;
+
+}
