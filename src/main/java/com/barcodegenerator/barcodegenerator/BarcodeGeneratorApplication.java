2c33d006b82919bb2aef4625af88c927145200ed
Vilius <polerisviliux@gmail.com>
2021-08-12 17:31:18 +0300

    Swagger added

--- src/main/java/com/barcodegenerator/barcodegenerator/BarcodeGeneratorApplication.java
+++ src/main/java/com/barcodegenerator/barcodegenerator/BarcodeGeneratorApplication.java
@@ -3,8 +3,10 @@ package com.barcodegenerator.barcodegenerator;
 import com.barcodegenerator.barcodegenerator.service.BarcodeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BarcodeGeneratorApplication {
 
     public static void main(String[] args) {
